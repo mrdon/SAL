0a0bff3048ce98e4dae44a7e365a19c49b973f77
dhornik <dhornik@atlassian.com>
2008-05-27 03:51:33 +0000

    JST-812 Messing around with Exceptions
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@16690 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ sal-api/src/main/java/com/atlassian/sal/api/net/ResponseException.java
@@ -0,0 +1,30 @@
+package com.atlassian.sal.api.net;
+
+/**
+ * This is exception is thrown by {@link Request#execute()} and {@link ResponseHandler#handle(Response)}. Acts as a wrapper for any IOException thrown when executing the request. 
+ * It also a root of user-defined exceptions thrown in {@link ResponseHandler#handle(Response)} method.
+ */
+public class ResponseException extends Exception
+{
+
+	public ResponseException()
+	{
+		super();
+	}
+
+	public ResponseException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public ResponseException(String message)
+	{
+		super(message);
+	}
+
+	public ResponseException(Throwable cause)
+	{
+		super(cause);
+	}
+
+}
