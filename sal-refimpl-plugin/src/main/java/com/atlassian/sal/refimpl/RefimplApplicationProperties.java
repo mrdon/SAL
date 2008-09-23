537ddd14103e792bc08df938a3d0a76b98de83f0
rwallace <rwallace@atlassian.com>
2008-09-23 18:05:47 +0000

    get the base url from a system property
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@20347 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-refimpl-plugin/src/main/java/com/atlassian/sal/refimpl/RefimplApplicationProperties.java
+++ sal-refimpl-plugin/src/main/java/com/atlassian/sal/refimpl/RefimplApplicationProperties.java
@@ -9,7 +9,7 @@ public class RefimplApplicationProperties implements ApplicationProperties
 {
     public String getBaseUrl()
     {
-        return "http://localhost";
+        return System.getProperty("baseurl", "http://localhost:8080/atlassian-plugins-refimpl");
     }
 
     public String getApplicationName()
