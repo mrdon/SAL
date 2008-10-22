f420cbe9f5516309a02168ab3ce5017422100d5b
dhornik <dhornik@atlassian.com>
2008-10-22 00:11:25 +0000

    Removing unused imports and constant
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@21297 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-fisheye-plugin/src/main/java/com/atlassian/sal/fisheye/user/DefaultUserManager.java
+++ sal-fisheye-plugin/src/main/java/com/atlassian/sal/fisheye/user/DefaultUserManager.java
@@ -2,22 +2,19 @@ package com.atlassian.sal.fisheye.user;
 
 import org.apache.log4j.Logger;
 
-import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.sal.api.component.ComponentLocator;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+import com.atlassian.sal.api.user.UserManager;
 import com.cenqua.crucible.filters.CrucibleFilter;
 import com.cenqua.fisheye.AppConfig;
 import com.cenqua.fisheye.LicensePolicyException;
 import com.cenqua.fisheye.rep.DbException;
 import com.cenqua.fisheye.user.UserLogin;
 
-import javax.servlet.http.HttpServletRequest;
-
 /**
  */
 public class DefaultUserManager implements UserManager
 {
-    private static final String ADMIN_PASSWORD_HEADER = "x-fisheye-admin-password";
     private static final Logger log = Logger.getLogger(DefaultUserManager.class);
 
     public String getRemoteUsername()
@@ -27,7 +24,7 @@ public class DefaultUserManager implements UserManager
         {
             user = AppConfig.getsConfig().getUserManager().getCurrentUser(CrucibleFilter.getRequest());
         }
-        catch (IllegalStateException ise)
+        catch (final IllegalStateException ise)
         {
             return null;
         }
@@ -44,13 +41,12 @@ public class DefaultUserManager implements UserManager
         {
             return false;
         }
-        String sysadmins = (String)
-        ComponentLocator.getComponent(PluginSettingsFactory.class).createGlobalSettings().get("sysadmins");
+        final String sysadmins = (String) ComponentLocator.getComponent(PluginSettingsFactory.class).createGlobalSettings().get("sysadmins");
         if (sysadmins == null)
         {
             return false;
         }
-        for (String sysadmin : sysadmins.split(","))
+        for (final String sysadmin : sysadmins.split(","))
         {
             if (username.equals(sysadmin))
             {
@@ -62,12 +58,12 @@ public class DefaultUserManager implements UserManager
 
     public boolean isUserInGroup(String username, String group)
     {
-        final com.cenqua.fisheye.user.UserManager um = AppConfig.getsConfig().getUserManager();
+        final com.cenqua.fisheye.user.UserManager userManager = AppConfig.getsConfig().getUserManager();
         try
         {
-            return um.isUserInGroup(group, username);
+            return userManager.isUserInGroup(group, username);
         }
-        catch (DbException e)
+        catch (final DbException e)
         {
             log.error("Database error trying to test users group: '" + username + "'", e);
         }
@@ -76,21 +72,20 @@ public class DefaultUserManager implements UserManager
 
     public boolean authenticate(String username, String password)
     {
-        final com.cenqua.fisheye.user.UserManager um = AppConfig.getsConfig().getUserManager();
+        final com.cenqua.fisheye.user.UserManager userManager = AppConfig.getsConfig().getUserManager();
         try
         {
-            final UserLogin userLogin = um.login(CrucibleFilter.getRequest(), CrucibleFilter.getResponse(), username,
-                password, false);
+            final UserLogin userLogin = userManager.login(CrucibleFilter.getRequest(), CrucibleFilter.getResponse(), username, password, false);
             if (userLogin != null)
             {
                 return true;
             }
         }
-        catch (DbException e)
+        catch (final DbException e)
         {
             log.error("Database error trying to authenticate user '" + username + "'", e);
         }
-        catch (LicensePolicyException e)
+        catch (final LicensePolicyException e)
         {
             log.error("License error trying to authenticate user '" + username + "'", e);
         }
