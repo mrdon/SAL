fdc550d60d8255f804287b60d492117492da0971
dhornik <dhornik@atlassian.com>
2008-11-19 05:33:16 +0000

    [maven-release-plugin]  copy for branch sal-1.0
    git-svn-id: https://studio.atlassian.com/svn/SAL/branches/sal-1.0@23568 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-crowd-plugin/src/main/java/com/atlassian/sal/crowd/lifecycle/CrowdLifecycleManager.java
+++ sal-crowd-plugin/src/main/java/com/atlassian/sal/crowd/lifecycle/CrowdLifecycleManager.java
@@ -9,4 +9,12 @@ public class CrowdLifecycleManager extends DefaultLifecycleManager
 	{
 		return BootstrapUtils.getBootstrapManager().isSetupComplete();
 	}
+	
+	@Override
+	protected boolean isStarted()
+	{
+		// Always return false for Crowd, because Crowd doesn't re-instantiate LifecycleManager after config import
+		// TODO It sounds like a hack and we should think of a better solution.
+		return false;
+	}
 }
