9cc9bd1d1b72bbe20fe5e3d116642d7bcf827802
dhornik <dhornik@atlassian.com>
2008-11-27 03:56:56 +0000

    JST-1269 Fixing PluginSettings test
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@23904 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-confluence-plugin/src/main/java/com/atlassian/sal/confluence/pluginsettings/ConfluencePluginSettings.java
+++ sal-confluence-plugin/src/main/java/com/atlassian/sal/confluence/pluginsettings/ConfluencePluginSettings.java
@@ -1,34 +1,43 @@
 package com.atlassian.sal.confluence.pluginsettings;
 
+import java.util.List;
+import java.util.Properties;
+
 import com.atlassian.bandana.BandanaManager;
 import com.atlassian.confluence.setup.bandana.ConfluenceBandanaContext;
 import com.atlassian.sal.api.pluginsettings.PluginSettings;
 
 public class ConfluencePluginSettings implements PluginSettings
 {
-    private BandanaManager bandanaManager;
-    private ConfluenceBandanaContext ctx;
+    private final BandanaManager bandanaManager;
+    private final ConfluenceBandanaContext ctx;
 
-    public ConfluencePluginSettings(BandanaManager bandanaManager, ConfluenceBandanaContext ctx)
+    public ConfluencePluginSettings(final BandanaManager bandanaManager, final ConfluenceBandanaContext ctx)
     {
         this.bandanaManager = bandanaManager;
         this.ctx = ctx;
     }
 
-    public Object put(String key, Object val)
+    public Object put(final String key, final Object val)
     {
-        bandanaManager.setValue(ctx, key, val);
-        return val;
+    	if ((val instanceof Properties) || (val instanceof List)  || (val instanceof String) )
+		{
+    		bandanaManager.setValue(ctx, key, val);
+    		return val;
+		} else
+		{
+            throw new IllegalArgumentException("Property type: "+val.getClass()+" not supported");
+		}
     }
 
-    public Object get(String key)
+    public Object get(final String key)
     {
         return bandanaManager.getValue(ctx, key.toString());
     }
 
-    public Object remove(String key)
+    public Object remove(final String key)
     {
-        Object val = get(key);
+        final Object val = get(key);
         put(key, null);
         return val;
     }
