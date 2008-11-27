48af2085d39cf5ed9a859806ff59e7f51066a615
jroper <jroper@atlassian.com>
2008-11-27 04:45:20 +0000

    Upgraded FishEye to 1.6 and fixed up broken build
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@23915 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-fisheye-plugin/src/main/java/com/atlassian/sal/fisheye/lifecycle/FisheyeLifecycleManager.java
+++ sal-fisheye-plugin/src/main/java/com/atlassian/sal/fisheye/lifecycle/FisheyeLifecycleManager.java
@@ -4,11 +4,19 @@ import com.cenqua.fisheye.AppConfig;
 import com.cenqua.fisheye.config.AdminConfig;
 import com.cenqua.fisheye.config.RootConfig;
 import com.atlassian.sal.core.lifecycle.DefaultLifecycleManager;
+import com.atlassian.sal.api.lifecycle.LifecycleAware;
+import com.atlassian.plugin.event.PluginEventManager;
+
+import java.util.List;
 
 public class FisheyeLifecycleManager extends DefaultLifecycleManager
 {
+    public FisheyeLifecycleManager(PluginEventManager eventManager, List<LifecycleAware> listeners)
+    {
+        super(eventManager, listeners);
+    }
 
-	public boolean isApplicationSetUp()
+    public boolean isApplicationSetUp()
 	{
 		// this code is copied from TotalityFilter.requresSetup() method
 		RootConfig rootConfig = AppConfig.getsConfig();
