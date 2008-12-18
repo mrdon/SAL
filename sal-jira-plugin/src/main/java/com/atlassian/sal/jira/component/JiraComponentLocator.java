f50bc1ddadfbbb1c78df8c9d52972e53f5d2a6c7
dhornik <dhornik@atlassian.com>
2008-12-18 01:58:49 +0000

    JST-1289 fixed JiraComponentLocator lookup by key
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@25057 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-jira-plugin/src/main/java/com/atlassian/sal/jira/component/JiraComponentLocator.java
+++ sal-jira-plugin/src/main/java/com/atlassian/sal/jira/component/JiraComponentLocator.java
@@ -8,45 +8,58 @@ import java.util.List;
 import org.picocontainer.ComponentAdapter;
 import org.picocontainer.PicoContainer;
 
+import uk.ltd.getahead.dwr.util.Logger;
+
 import com.atlassian.jira.ComponentManager;
 import com.atlassian.sal.api.component.ComponentLocator;
 
 public class JiraComponentLocator extends ComponentLocator
 {
+    private static final Logger log = Logger.getLogger(JiraComponentLocator.class);
     public JiraComponentLocator()
     {
         ComponentLocator.setComponentLocator(this);
     }
 
+    @Override
     @SuppressWarnings("unchecked")
-	protected <T> T getComponentInternal(Class<T> iface)
+	protected <T> T getComponentInternal(final Class<T> iface)
     {
         return (T) ComponentManager.getComponentInstanceOfType(iface);
     }
 
 	@Override
-	protected <T> Collection<T> getComponentsInternal(Class<T> iface)
+	protected <T> Collection<T> getComponentsInternal(final Class<T> iface)
 	{
-		Collection<T> implementations = new ArrayList<T>();
-		PicoContainer picoContainer = ComponentManager.getInstance().getContainer();
-		List componentAdaptersOfType = picoContainer.getComponentAdaptersOfType(iface);
-		for (Iterator iterator = componentAdaptersOfType.iterator(); iterator.hasNext();)
+		final Collection<T> implementations = new ArrayList<T>();
+		final PicoContainer picoContainer = ComponentManager.getInstance().getContainer();
+		final List componentAdaptersOfType = picoContainer.getComponentAdaptersOfType(iface);
+		for (final Iterator iterator = componentAdaptersOfType.iterator(); iterator.hasNext();)
 		{
-			ComponentAdapter componentAdapter = (ComponentAdapter) iterator.next();
+			final ComponentAdapter componentAdapter = (ComponentAdapter) iterator.next();
 			implementations.add((T) componentAdapter.getComponentInstance());
 		}
 		return implementations;
 	}
 
 	@Override
-	protected <T> T getComponentInternal(Class<T> iface, String componentKey)
+	protected <T> T getComponentInternal(final Class<T> iface, final String componentKey)
 	{
-		PicoContainer picoContainer = ComponentManager.getInstance().getContainer();
-		List componentAdaptersOfType = picoContainer.getComponentAdaptersOfType(iface);
-		for (Iterator iterator = componentAdaptersOfType.iterator(); iterator.hasNext();)
+	    final Class<?> key;
+	    try
+        {
+            key = Class.forName(componentKey);
+        } catch (final ClassNotFoundException e)
+        {
+            log.warn(e.getMessage(),e);
+            return null;
+        }
+		final PicoContainer picoContainer = ComponentManager.getInstance().getContainer();
+		final List componentAdaptersOfType = picoContainer.getComponentAdaptersOfType(iface);
+		for (final Iterator iterator = componentAdaptersOfType.iterator(); iterator.hasNext();)
 		{
-			ComponentAdapter componentAdapter = (ComponentAdapter) iterator.next();
-			if (componentKey.equals(componentAdapter.getComponentKey()))
+			final ComponentAdapter componentAdapter = (ComponentAdapter) iterator.next();
+			if (key.equals(componentAdapter.getComponentKey()))
 					return (T) componentAdapter.getComponentInstance();
 		}
 		return null;
