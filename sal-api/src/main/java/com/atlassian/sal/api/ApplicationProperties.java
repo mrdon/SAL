c3c83f68d457130f9391ff69662348e909fb9972
sleberrigaud <sleberrigaud@atlassian.com>
2009-02-02 03:31:53 +0000

    SAL-18: Added @since tags. As per review comments.
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@27252 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-api/src/main/java/com/atlassian/sal/api/ApplicationProperties.java
+++ sal-api/src/main/java/com/atlassian/sal/api/ApplicationProperties.java
@@ -9,30 +9,31 @@ public interface ApplicationProperties
 {
     /**
      * Get the base URL of the current application.
-     *
      * @return the current application's base URL
      */
     String getBaseUrl();
 
     /**
      * Returns one of JIRA, Confluence or FishEye
-     *
      * @return one of JIRA, Confluence or FishEye
      */
     String getApplicationName();
 
     /**
      * Returns the version of the application
+     * @since 2.0.0
      */
     String getVersion();
 
     /**
      * Returns the build date of the application
+     * @since 2.0.0
      */
     Date getBuildDate();
 
     /**
      * Returns the build number of the application
+     * @since 2.0.0
      */
     String getBuildNumber();
 }
