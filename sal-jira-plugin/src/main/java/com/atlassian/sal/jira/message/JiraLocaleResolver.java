adaec1cb1cc4659034eb299874e6866e383cb1e2
andreask <andreask@atlassian.com>
2009-02-24 22:13:11 +0000

    SAL-29 & AG-375: Moved to new and better implementation of the LocaleManager in JIRA.
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@29025 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-jira-plugin/src/main/java/com/atlassian/sal/jira/message/JiraLocaleResolver.java
+++ sal-jira-plugin/src/main/java/com/atlassian/sal/jira/message/JiraLocaleResolver.java
@@ -1,22 +1,19 @@
 package com.atlassian.sal.jira.message;
 
+import com.atlassian.jira.config.LocaleManager;
 import com.atlassian.jira.web.bean.I18nBean;
-import com.atlassian.jira.web.util.LocaleManager;
 import com.atlassian.sal.api.message.LocaleResolver;
 import com.atlassian.seraph.auth.DefaultAuthenticator;
 import com.opensymphony.user.User;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
- * Resolves the locale for a particular request.  Depends on the user that's currently logged in,
- * otherwise the system default locale will be used. 
+ * Resolves the locale for a particular request.  Depends on the user that's currently logged in, otherwise the system
+ * default locale will be used.
  *
  * @since v2.1
  */
@@ -37,17 +34,14 @@ public class JiraLocaleResolver implements LocaleResolver
     }
 
     public Set<Locale> getSupportedLocales()
-    {        
-        @SuppressWarnings("unchecked")
-        final List<Locale> list = localeManager.getInstalledLocales();
-
-        return Collections.unmodifiableSet(new HashSet<Locale>(list));
+    {
+        return localeManager.getInstalledLocales();
     }
 
     private User getUser(final HttpServletRequest request)
     {
         final HttpSession session = request.getSession(false);
-        if(session != null)
+        if (session != null)
         {
             return (User) session.getAttribute(DefaultAuthenticator.LOGGED_IN_KEY);
         }
