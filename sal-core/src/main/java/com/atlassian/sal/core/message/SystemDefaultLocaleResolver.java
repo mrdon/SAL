bacbdf60f2e58ae5fc0240ce50035444b9f112de
dbrown <dbrown@atlassian.com>
2009-03-24 03:41:12 +0000

    Add LocaleResolver impls for all products that don't support locales
    git-svn-id: https://studio.atlassian.com/svn/SAL/branches/2.0-stable@31173 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-core/src/main/java/com/atlassian/sal/core/message/SystemDefaultLocaleResolver.java
+++ sal-core/src/main/java/com/atlassian/sal/core/message/SystemDefaultLocaleResolver.java
@@ -1,19 +1,19 @@
-package com.atlassian.sal.crowd.message;
+package com.atlassian.sal.core.message;
 
 import com.atlassian.sal.api.message.LocaleResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
 import java.util.Set;
-import java.util.Collections;
 import java.util.HashSet;
+import java.util.Collections;
 
 /**
- * Crowd resolver that only supports the system default locale.
+ * Simple resolver that only supports the system default locale.
  *
  * @since 2.2.0
  */
-public class CrowdLocaleResolver implements LocaleResolver
+public class SystemDefaultLocaleResolver implements LocaleResolver
 {
     public Locale getLocale(HttpServletRequest request)
     {
