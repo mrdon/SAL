48a5bb95b2ea7c56c5f9f3d4e553599dfe7f3afc
dhornik <dhornik@atlassian.com>
2009-03-24 04:08:04 +0000

    Adding some comments
    git-svn-id: https://studio.atlassian.com/svn/SAL/branches/2.0-stable@31195 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-crowd-plugin/src/main/java/com/atlassian/sal/crowd/auth/CrowdLoginUriProvider.java
+++ sal-crowd-plugin/src/main/java/com/atlassian/sal/crowd/auth/CrowdLoginUriProvider.java
@@ -6,9 +6,16 @@ import com.atlassian.sal.api.auth.LoginUriProvider;
 
 public class CrowdLoginUriProvider implements LoginUriProvider
 {
+//    private final ClientProperties clientProperties;
+//
+//    public CrowdLoginUriProvider(final ClientProperties clientProperties)
+//    {
+//        this.clientProperties = clientProperties;
+//    }
 
     public URI getLoginUri(final URI returnUri)
     {
+//        clientProperties.getApplicationAuthenticationURL();
         throw new UnsupportedOperationException("LoginUriProvider in Crowd is not implemented yet");
     }
 
