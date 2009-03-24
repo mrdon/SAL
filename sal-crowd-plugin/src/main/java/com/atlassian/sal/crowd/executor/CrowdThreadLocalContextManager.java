a3a46c59c544721e2f27fb85d5c4e1c4788eb695
dbrown <dbrown@atlassian.com>
2009-03-24 03:41:45 +0000

    Add thread local executor factory that just throws unsupported operation exceptions for Crowd
    git-svn-id: https://studio.atlassian.com/svn/SAL/branches/2.0-stable@31176 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-crowd-plugin/src/main/java/com/atlassian/sal/crowd/executor/CrowdThreadLocalContextManager.java
+++ sal-crowd-plugin/src/main/java/com/atlassian/sal/crowd/executor/CrowdThreadLocalContextManager.java
@@ -2,22 +2,25 @@ package com.atlassian.sal.crowd.executor;
 
 import com.atlassian.sal.core.executor.ThreadLocalContextManager;
 
+/**
+ * Crowd context manager that just throws {@link UnsupportedOperationException}s
+ *
+ * @since 2.0.0
+ */
 public class CrowdThreadLocalContextManager implements ThreadLocalContextManager
 {
+    public Object getThreadLocalContext()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setThreadLocalContext(Object context)
+    {
+        throw new UnsupportedOperationException();
+    }
 
     public void clearThreadLocalContext()
     {
-        throw new UnsupportedOperationException("CrowdThreadLocalContextManager in Crowd is not implemented.");
+        throw new UnsupportedOperationException();
     }
-
-    public Object getThreadLocalContext()
-    {
-        throw new UnsupportedOperationException("CrowdThreadLocalContextManager in Crowd is not implemented.");
-    }
-
-    public void setThreadLocalContext(final Object context)
-    {
-        throw new UnsupportedOperationException("CrowdThreadLocalContextManager in Crowd is not implemented.");
-    }
-
 }
--- sal-crowd-plugin/src/main/java/com/atlassian/sal/crowd/executor/CrowdThreadLocalDelegateExecutorFactory.java
+++ sal-crowd-plugin/src/main/java/com/atlassian/sal/crowd/executor/CrowdThreadLocalDelegateExecutorFactory.java
@@ -2,6 +2,11 @@ package com.atlassian.sal.crowd.executor;
 
 import com.atlassian.sal.core.executor.DefaultThreadLocalDelegateExecutorFactory;
 
+/**
+ * Simple executor factory that uses the crowd context local manager
+ *
+ * @since 2.0.0
+ */
 public class CrowdThreadLocalDelegateExecutorFactory extends DefaultThreadLocalDelegateExecutorFactory
 {
     public CrowdThreadLocalDelegateExecutorFactory()
