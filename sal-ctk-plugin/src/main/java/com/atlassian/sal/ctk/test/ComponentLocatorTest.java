0b8c59a6b2cd9153d4a571d690ea17d7a0c0140e
dbrown <dbrown@atlassian.com>
2009-05-08 02:31:56 +0000

    Add test for PluginAccessor
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@35212 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-ctk-plugin/src/main/java/com/atlassian/sal/ctk/test/ComponentLocatorTest.java
+++ sal-ctk-plugin/src/main/java/com/atlassian/sal/ctk/test/ComponentLocatorTest.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.stereotype.Component;
 
 import com.atlassian.plugin.PluginController;
+import com.atlassian.plugin.PluginAccessor;
 import com.atlassian.sal.api.component.ComponentLocator;
 import com.atlassian.sal.ctk.CtkTest;
 import com.atlassian.sal.ctk.CtkTestResults;
@@ -25,7 +26,14 @@ public class ComponentLocatorTest implements CtkTest
             results.assertTrue("Should be one PluginController found", c != null && !c.isEmpty());
             results.assertTrue("There should be only one PluginController", c.size()==1);
 
-            results.assertTrueOrWarn("HostContextAccessor should be accessible in ComponentLocator unless the application provides its own ComponentLocator and ", ComponentLocator.getComponent(HostContextAccessor.class) != null);
+            final PluginAccessor accessor = ComponentLocator.getComponent(PluginAccessor.class);
+            results.assertTrue("PluginAccessor accessible in ComponentLocator", accessor != null);
+
+            final Collection<PluginAccessor> ca = ComponentLocator.getComponents(PluginAccessor.class);
+            results.assertTrue("Should be one PluginAccessor found", ca != null && !ca.isEmpty());
+            results.assertTrue("There should be only one PluginAccessor", ca.size()==1);
+
+            results.assertTrueOrWarn("PluginAccessor should be accessible in ComponentLocator", ComponentLocator.getComponent(PluginAccessor.class) != null);
         } catch (final UnsupportedOperationException ex)
         {
             results.fail("ComponentLocator operations should be supported");
