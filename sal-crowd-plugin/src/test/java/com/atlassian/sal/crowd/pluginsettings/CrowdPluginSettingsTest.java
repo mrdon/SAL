84ab43d863b84b4add9590bb740869a6121518f6
jkoke <jkoke@atlassian.com>
2009-05-18 07:56:31 +0000

    Commiting fix so the tests pass for SAL-DEF-1146 and SAL-57
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@36067 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-crowd-plugin/src/test/java/com/atlassian/sal/crowd/pluginsettings/CrowdPluginSettingsTest.java
+++ sal-crowd-plugin/src/test/java/com/atlassian/sal/crowd/pluginsettings/CrowdPluginSettingsTest.java
@@ -1,55 +1,49 @@
 package com.atlassian.sal.crowd.pluginsettings;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import junit.framework.TestCase;
-
-import org.springframework.dao.DataAccessException;
-
 import com.atlassian.crowd.integration.exception.ObjectNotFoundException;
-import com.atlassian.crowd.model.salproperty.SALProperty;
-import com.atlassian.crowd.model.salproperty.SALPropertyDAO;
+import com.atlassian.crowd.manager.property.PluginPropertyManager;
+import junit.framework.TestCase;
+import static org.mockito.Mockito.*;
+import org.springframework.dao.DataAccessException;
 
 public class CrowdPluginSettingsTest extends TestCase
 {
-	@Override
-	protected void setUp() throws Exception
-	{
-		super.setUp();
-	}
-	
-	public void testPutActual()
-	{
-		final SALPropertyDAO mockDAO = mock(SALPropertyDAO.class);
-		CrowdPluginSettings crowdPluginSettings = new CrowdPluginSettings(null, mockDAO);
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+    }
 
-		// 3. not null key, not null propertyName, not null value.
-		crowdPluginSettings.put("hi", "hello");
-		verify(mockDAO).saveOrUpdate(new SALProperty("null","hi","hello"));	// note that SALProperty#equals ignores "hello" anyway
+    public void testPutActual()
+    {
+        final PluginPropertyManager mockPluginPropertyManager = mock(PluginPropertyManager.class);
+        CrowdPluginSettings crowdPluginSettings = new CrowdPluginSettings(null, mockPluginPropertyManager);
 
-		// 3. not null key, not null propertyName, not null value. 
-		crowdPluginSettings = new CrowdPluginSettings("key", mockDAO);
-		crowdPluginSettings.put("hi", "hello");
-		verify(mockDAO).saveOrUpdate(new SALProperty("key","hi","hello"));	// note that SALProperty#equals ignores "hello" anyway
-	}
+        // 3. not null key, not null propertyName, not null value.
+        crowdPluginSettings.put("hi", "hello");
+        verify(mockPluginPropertyManager).setProperty("null", "hi", "hello");    // note that SALProperty#equals ignores "hello" anyway
 
-	public void testGetActual() throws DataAccessException, ObjectNotFoundException
-	{
-		final SALPropertyDAO mockDAO = mock(SALPropertyDAO.class);
-		CrowdPluginSettings crowdPluginSettings = new CrowdPluginSettings(null, mockDAO);
-		final SALProperty someSalProperty = new SALProperty(null, null, null);
+        // 3. not null key, not null propertyName, not null value.
+        crowdPluginSettings = new CrowdPluginSettings("key", mockPluginPropertyManager);
+        crowdPluginSettings.put("hi", "hello");
+        verify(mockPluginPropertyManager).setProperty("key", "hi", "hello");    // note that SALProperty#equals ignores "hello" anyway
+    }
+
+    public void testGetActual() throws DataAccessException, ObjectNotFoundException
+    {
+        final PluginPropertyManager mockPluginPropertyManager = mock(PluginPropertyManager.class);
+        CrowdPluginSettings crowdPluginSettings = new CrowdPluginSettings(null, mockPluginPropertyManager);
 
         // 2. not null key, not null propertyName
-		doReturn(someSalProperty).when(mockDAO).find("null","hi");
-		crowdPluginSettings.get("hi");
-		verify(mockDAO).find("null","hi");
-		
-		// 3. not null key, not null propertyName, not null value. 
-		doReturn(someSalProperty).when(mockDAO).find("key","hi");
-		crowdPluginSettings = new CrowdPluginSettings("key", mockDAO);
-		crowdPluginSettings.get("hi");
-		verify(mockDAO).find("key","hi");
-	}
+        doReturn("random property").when(mockPluginPropertyManager).getProperty("null", "hi");
+        crowdPluginSettings.get("hi");
+        verify(mockPluginPropertyManager).getProperty("null", "hi");
+
+        // 3. not null key, not null propertyName, not null value.
+        doReturn("random property").when(mockPluginPropertyManager).getProperty("key", "hi");
+        crowdPluginSettings = new CrowdPluginSettings("key", mockPluginPropertyManager);
+        crowdPluginSettings.get("hi");
+        verify(mockPluginPropertyManager).getProperty("key", "hi");
+    }
 
 }
