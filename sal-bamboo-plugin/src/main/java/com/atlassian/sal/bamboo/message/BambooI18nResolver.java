7bb79f71e343fe88c31e073b5542a94b699b98fa
sleberrigaud <sleberrigaud@atlassian.com>
2009-05-26 10:52:11 +0000

    BAM-3933 Use new Bamboo's i18n bean
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@36874 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-bamboo-plugin/src/main/java/com/atlassian/sal/bamboo/message/BambooI18nResolver.java
+++ sal-bamboo-plugin/src/main/java/com/atlassian/sal/bamboo/message/BambooI18nResolver.java
@@ -1,28 +1,28 @@
 package com.atlassian.sal.bamboo.message;
 
 import org.apache.log4j.Logger;
+import com.atlassian.bamboo.utils.i18n.I18nBeanFactory;
+import com.atlassian.bamboo.utils.i18n.I18nBean;
 import com.atlassian.sal.core.message.AbstractI18nResolver;
-import com.opensymphony.xwork.TextProvider;
-import com.opensymphony.xwork.util.LocalizedTextUtil;
 
 import java.io.Serializable;
 import java.util.*;
 
 public class BambooI18nResolver extends AbstractI18nResolver
 {
+    @SuppressWarnings({"UnusedDeclaration"})
     private static final Logger log = Logger.getLogger(BambooI18nResolver.class);
     // ------------------------------------------------------------------------------------------------------- Constants
-    private static final String DEFAULT_BUNDLE = "com.atlassian.bamboo.ww2.BambooActionSupport";
-
     // ------------------------------------------------------------------------------------------------- Type Properties
     // ---------------------------------------------------------------------------------------------------- Dependencies
-    private final TextProvider textProvider;
+
+    private final I18nBeanFactory i18nBeanFactory;
 
     // ---------------------------------------------------------------------------------------------------- Constructors
 
-    public BambooI18nResolver(TextProvider textProvider)
+    public BambooI18nResolver(I18nBeanFactory i18nBeanFactory)
     {
-        this.textProvider = textProvider;
+        this.i18nBeanFactory = i18nBeanFactory;
     }
 
     // ----------------------------------------------------------------------------------------------- Interface Methods
@@ -30,43 +30,30 @@ public class BambooI18nResolver extends AbstractI18nResolver
     // -------------------------------------------------------------------------------------------------- Public Methods
     public String resolveText(String key, Serializable[] arguments)
     {
-        String text = textProvider.getText(key, Arrays.asList(arguments));
-        if (text != null)
-        {
-            return text;
-        }
-        else
-        {
-            return key;
-        }
+        return getI18nBean().getText(key, arguments);
     }
 
-    // @TODO This is a crap version of the translator that doesn't allow plugins to plugin their own messages
     public Map<String, String> getAllTranslationsForPrefix(String prefix, Locale locale)
     {
-        final Map<String, String> messages = new HashMap<String, String>();
-        final ResourceBundle bundle = textProvider.getTexts(DEFAULT_BUNDLE);
-        if (bundle != null)
-        {
-            final Enumeration<String> keys = bundle.getKeys();
-            while (keys.hasMoreElements())
-            {
-                String key = keys.nextElement();
-                if (key.startsWith(prefix))
-                {
-                    messages.put(key, bundle.getString(key));
-                }
-            }
-        }
-        else
-        {
-            // Pretty sure this never happens...
-            log.warn("Unable to find bundle " + DEFAULT_BUNDLE + ". No i18n messages will be returned");
-        }
-
-        return messages;
+        return getI18nBean(locale).getAllTranslationsForPrefix(prefix);
     }
 
-    // ------------------------------------------------------------------------------------------------- Helper Methods
+    // -------------------------------------------------------------------------------------------------- Helper Methods
+
+    private Locale getLocale()
+    {
+        return Locale.getDefault();
+    }
+
+    private I18nBean getI18nBean()
+    {
+        return getI18nBean(getLocale());
+    }
+
+    private I18nBean getI18nBean(Locale locale)
+    {
+        return i18nBeanFactory.getI18nBean(locale);
+    }
+
     // -------------------------------------------------------------------------------------- Basic Accessors / Mutators
 }
