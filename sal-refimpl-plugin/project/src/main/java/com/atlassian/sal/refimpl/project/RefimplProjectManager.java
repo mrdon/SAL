fe453febf9609a2e5f0fefeedde0c9798251aa81
jroper <jroper@atlassian.com>
2009-05-28 10:11:10 +0000

    Merged in charlie support
    git-svn-id: https://studio.atlassian.com/svn/SAL/branches/2.1-net-request-entity@37069 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-refimpl-plugin/project/src/main/java/com/atlassian/sal/refimpl/project/RefimplProjectManager.java
+++ sal-refimpl-plugin/project/src/main/java/com/atlassian/sal/refimpl/project/RefimplProjectManager.java
@@ -1,13 +1,21 @@
 package com.atlassian.sal.refimpl.project;
 
-import java.util.Arrays;
-import java.util.Collection;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 
-/**
- * Returns "FOO" and "BAR"
- */
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+
 public class RefimplProjectManager implements com.atlassian.sal.api.project.ProjectManager
 {
+    private static final String CHARLIE_KEYS = "charlie.keys";
+
+    private final PluginSettingsFactory pluginSettingsFactory;
+
+    public RefimplProjectManager(PluginSettingsFactory pluginSettingsFactory)
+    {
+        this.pluginSettingsFactory = pluginSettingsFactory;
+    }
 
     /**
      * Get all project keys
@@ -16,7 +24,12 @@ public class RefimplProjectManager implements com.atlassian.sal.api.project.Proj
      */
     public Collection<String> getAllProjectKeys()
     {
-        return Arrays.asList("FOO", "BAR");
+        List<String> charlies = (List<String>) pluginSettingsFactory.createGlobalSettings().get(CHARLIE_KEYS);
+        if (charlies == null)
+        {
+            charlies = new ArrayList<String>();
+        }
+        return charlies;
     }
 
 }
