c25df447263e14f6ef7616ea03749ce548faedf1
jpendleton <jpendleton@atlassian.com>
2009-09-15 00:34:46 +0000

    improve javadoc for lifecycleaware to say what you have to do to make it work
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@43943 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-api/src/main/java/com/atlassian/sal/api/lifecycle/LifecycleAware.java
+++ sal-api/src/main/java/com/atlassian/sal/api/lifecycle/LifecycleAware.java
@@ -2,6 +2,9 @@ package com.atlassian.sal.api.lifecycle;
 
 /**
  * Marks a class that wants to execute code on certain application-level lifecycle stages
+ * This only works on public components. This means the class must be listed as a component
+ * in the atlassian-plugins.xml and must be marked as public="true" and include an
+ * interface="com.atlassian.sal.api.lifecycle.LifecycleAware" section
  *
  * @since 2.0
  */
