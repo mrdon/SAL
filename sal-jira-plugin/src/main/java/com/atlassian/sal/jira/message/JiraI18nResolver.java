e2c70141d0ac69785647e1ab95b944b1aa0d1b2d
dhornik <dhornik@atlassian.com>
2009-12-01 01:37:45 +0000

    JST-1979 #resolve Merging back improvements in JiraI18nResolver
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@48807 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-jira-plugin/src/main/java/com/atlassian/sal/jira/message/JiraI18nResolver.java
+++ sal-jira-plugin/src/main/java/com/atlassian/sal/jira/message/JiraI18nResolver.java
@@ -1,18 +1,16 @@
 package com.atlassian.sal.jira.message;
 
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.jira.util.I18nHelper;
+import static com.atlassian.jira.util.dbc.Assertions.notNull;
+import com.atlassian.sal.core.message.AbstractI18nResolver;
+
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.Enumeration;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.ResourceBundle;
-
-import com.atlassian.jira.security.JiraAuthenticationContext;
-import com.atlassian.jira.util.I18nHelper;
-import com.atlassian.jira.util.dbc.Assertions;
-import com.atlassian.jira.web.bean.I18nBean;
-import com.atlassian.sal.core.message.AbstractI18nResolver;
+import java.util.Set;
 
 /**
  * A JIRA I18nResolver.  Uses the user's locale if a user is logged in or the default
@@ -29,36 +27,26 @@ public class JiraI18nResolver extends AbstractI18nResolver
         this.beanFactory = beanFactory;
     }
 
-    @Override
-    public String resolveText(final String key, final Serializable[] arguments)
+    public String resolveText(String key, Serializable[] arguments)
     {
         final I18nHelper bean = beanFactory.getInstance(jiraAuthenticationContext.getLocale());
         return bean.getText(key, arguments);
     }
 
-    public Map<String, String> getAllTranslationsForPrefix(final String prefix, final Locale locale)
+   public Map<String, String> getAllTranslationsForPrefix(final String prefix, final Locale locale)
     {
-        Assertions.notNull("prefix", prefix);
+        notNull("prefix", prefix);
+        notNull("locale", locale);
 
-        //if no locale was provided, fall back to the default locale.
-        final I18nBean i18nBean = locale == null ? new I18nBean() : new I18nBean(locale);
-        final Collection<ResourceBundle> bundles = i18nBean.getBundles();
+        final I18nHelper i18nBean = beanFactory.getInstance(locale);
+        final Set<String> keys = i18nBean.getKeysForPrefix(prefix);
 
         final Map<String, String> ret = new HashMap<String, String>();
-        //loop through all resource bundles, and find all keys with the given prefix.
-        for (final ResourceBundle bundle : bundles)
+        for (String key : keys)
         {
-            final Enumeration<String> keys = bundle.getKeys();
-            while (keys.hasMoreElements())
-            {
-                final String key = keys.nextElement();
-                if (key.startsWith(prefix))
-                {
-                    ret.put(key, bundle.getString(key));
-                }
-            }
+            ret.put(key, i18nBean.getText(key));
         }
 
-        return ret;
+        return Collections.unmodifiableMap(ret);
     }
 }
