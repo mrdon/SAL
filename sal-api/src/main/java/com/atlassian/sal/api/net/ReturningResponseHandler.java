7ef130410e468910da61247ef39d15968650906b
evzijst <evzijst@atlassian.com>
2010-07-29 23:45:14 +0000

    SAL-123: Add a new Request.execute(ResponseHandler) method that returns a generic result Added more javadoc.
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@65012 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-api/src/main/java/com/atlassian/sal/api/net/ReturningResponseHandler.java
+++ sal-api/src/main/java/com/atlassian/sal/api/net/ReturningResponseHandler.java
@@ -1,7 +1,15 @@
 package com.atlassian.sal.api.net;
 
 /**
- * @since   v2.2.0
+ * Callback interface used by the {@link Request#executeAndReturn(ReturningResponseHandler)}
+ * method. Implementation of this interface performs actual handling of the
+ * response.
+ * <p>
+ * If no result will be produced, then instead of using this class, implement
+ * {@link com.atlassian.sal.api.net.ResponseHandler} and pass it to
+ * {@link Request#execute(ResponseHandler)}.
+ *
+ * @since   2.2
  */
 public interface ReturningResponseHandler<T extends Response, R>
 {
@@ -10,6 +18,7 @@ public interface ReturningResponseHandler<T extends Response, R>
      * method becomes available. Implementations of this method should handle the response.
      *
      * @param response a response object. Never {@code null}.
+     * @return the result produces by this handler.
      * @throws ResponseException If the response cannot be retrieved
      */
     R handle(T response) throws ResponseException;
