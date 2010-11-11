2d72ca3b5da02ae40e80ba1e05a1f7e6050e7135
psongsiritat <psongsiritat@atlassian.com>
2010-11-11 23:50:15 +0000

    SAL-136: updated LocalResolver javadoc (applied from trunk)
    git-svn-id: https://studio.atlassian.com/svn/SAL/branches/sal-2.2.x@111215 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-api/src/main/java/com/atlassian/sal/api/message/LocaleResolver.java
+++ sal-api/src/main/java/com/atlassian/sal/api/message/LocaleResolver.java
@@ -13,9 +13,9 @@ public interface LocaleResolver
 {
     /**
      * Given a request, resolve the {@link Locale} that should be used in internationalization and localization.
-     * The locale should be determined by first checking the remote users preferences, then checking the preferred 
-     * locale as specified in the request and finally defaulting to the system locale if no preferred locale is set.  
-     * 
+     * The locale should be determined by first checking the remote users preferences, then defaulting to the
+     * application default locale if no preferred locale is set.
+     *
      * @param request Request to check 
      * @return Locale to be used in i18n and l10n. {@link Locale#getDefault()} if none found.
      */
