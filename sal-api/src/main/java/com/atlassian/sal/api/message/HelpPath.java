aad078f6a5dee1c8a30730ae1af524952467aa96
tcampbell <tcampbell@atlassian.com>
2010-11-15 21:55:09 +0000

    SAL-138 Updated documentation following code review.
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@111291 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-api/src/main/java/com/atlassian/sal/api/message/HelpPath.java
+++ sal-api/src/main/java/com/atlassian/sal/api/message/HelpPath.java
@@ -7,28 +7,32 @@ package com.atlassian.sal.api.message;
 public interface HelpPath
 {
     /**
-     * Alternate text for the help icon.
-     * @return Alternate text for the help icon.
-     */
-    String getAlt();
-
-    /**
-     * Title attribute on the help link (the tooltip).
-     * @return Title attribute on the help link
-     */
-    String getTitle();
-
-    /**
      * The key to the help item.
+     * This method should never return null.  If there is no URL for a key or the key is null then the {@link HelpPathResolver}
+     * should return null from the getHelpPath() method.
      * @return The key to the help item.
      */
     String getKey();
 
     /**
      * The full URL to the help content for this link.
+     * This should never return null.   If there is no URL for a key then the {@link HelpPathResolver} should return null from
+     * the getHelpPath() method.
      * @return The full URL to the help content for this link.
      */
     String getUrl();
+
+    /**
+     * Title attribute on the help link (the tooltip).
+     * @return Title attribute on the help link or null if there is none provided.
+     */
+    String getTitle();
+
+    /**
+     * Alternate text for the help icon.
+     * @return Alternate text for the help icon or null if there is none provided.
+     */
+    String getAlt();
 
     /**
      * Flag to indicate this is a local URL,
--- sal-api/src/main/java/com/atlassian/sal/api/message/HelpPathResolver.java
+++ sal-api/src/main/java/com/atlassian/sal/api/message/HelpPathResolver.java
@@ -3,6 +3,9 @@ package com.atlassian.sal.api.message;
 /**
  * Help path resolver.
  * This is used to retrieve product specific help.
+ * Plugins used with multiple Atlassian products may wish to have help integrated with the core product help,
+ * e.g. The administration plugin for embedded crowd directory management has different facilities in JIRA and Confluence
+ * and both JIRA and Confluence supply their own help pages for the plugin.
  *
  * @since 2.4
  */
@@ -10,9 +13,8 @@ public interface HelpPathResolver
 {
     /**
      * Retrieve the help path for a key.
-     * Implementers may choose to return null if no help is available, or if they prefer they can choose to
-     * return a default value.
-     * @param key Key to the help path.
+     * Implementers may choose to return null if no help is available for the given key.
+     * @param key Key to the help path.  The key passed in may be null.
      * @return HelpPath for the key.
      */
     HelpPath getHelpPath(String key);
