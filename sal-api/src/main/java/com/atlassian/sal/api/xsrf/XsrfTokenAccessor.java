2538bdf25bd944dbbbfb68a1f61f089f9ba2e4bb
jroper <jroper@atlassian.com>
2010-12-23 05:57:27 +0000

    SAL-142 Added XSRF token support #resolve
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@113297 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ sal-api/src/main/java/com/atlassian/sal/api/xsrf/XsrfTokenAccessor.java
@@ -0,0 +1,24 @@
+package com.atlassian.sal.api.xsrf;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Gives access to the applications XSRF tokens
+ *
+ * @since v2.4
+ */
+public interface XsrfTokenAccessor
+{
+    /**
+     * Get the persistent token for the given request, that is, the token stored in the users session or in a cookie,
+     * not the token submitted as part of a form.
+     *
+     * @param request The request to get the token from
+     * @param response The response to add the cookie to if necessary
+     * @param create Whether a new token should be created if there is none in the request.  The new token should be
+     *               persistent across subsequent requests, ie, added to the users session or a cookie.
+     * @return The token for the request, or null if no token was found and create was false
+     */
+    String getXsrfToken(HttpServletRequest request, HttpServletResponse response, boolean create);
+}
---
+++ sal-core/src/test/java/com/atlassian/sal/core/xsrf/TestIndependentXsrfTokenAccessor.java
@@ -0,0 +1,62 @@
+package com.atlassian.sal.core.xsrf;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.Cookie;
+
+import static com.atlassian.sal.core.xsrf.IndependentXsrfTokenAccessor.XSRF_COOKIE_KEY;
+import static org.junit.Assert.*;
+
+/**
+ * @since v2.4
+ */
+public class TestIndependentXsrfTokenAccessor
+{
+    private IndependentXsrfTokenAccessor accessor;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    @Before
+    public void setUp()
+    {
+        accessor = new IndependentXsrfTokenAccessor();
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+    }
+
+    @Test
+    public void testGetExistingToken()
+    {
+        request.setCookies(new Cookie[] {new Cookie(XSRF_COOKIE_KEY, "cookievalue")});
+        assertEquals("cookievalue", accessor.getXsrfToken(request, response, true));
+        assertNull(response.getCookie(XSRF_COOKIE_KEY));
+    }
+
+    @Test
+    public void testGetExistingTokenNoCreate()
+    {
+        request.setCookies(new Cookie[] {new Cookie(XSRF_COOKIE_KEY, "cookievalue")});
+        assertEquals("cookievalue", accessor.getXsrfToken(request, response, false));
+        assertNull(response.getCookie(XSRF_COOKIE_KEY));
+    }
+
+    @Test
+    public void testCreateToken()
+    {
+        String token = accessor.getXsrfToken(request, response, true);
+        assertNotNull(token);
+        Cookie cookie = response.getCookie(XSRF_COOKIE_KEY);
+        assertNotNull(cookie);
+        assertEquals(token, cookie.getValue());
+    }
+
+    @Test
+    public void testGetTokenNoCreate()
+    {
+        assertNull(accessor.getXsrfToken(request, response, false));
+        assertNull(response.getCookie(XSRF_COOKIE_KEY));
+    }
+}
