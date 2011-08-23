8a6753bc5e62b1e27e1cc94c757d7cb8c61106db
abhalla <abhalla@atlassian.com>
2011-08-23 00:33:50 +0000

    SAL-175: Add LicenseHandler.getServerId() and LicenseHandler.getSupportEntitlementNumber()
    git-svn-id: https://studio.atlassian.com/svn/SAL/trunk@128788 2c54a935-e501-0410-bc05-97a93f6bca70

--- sal-api/src/main/java/com/atlassian/sal/api/license/LicenseHandler.java
+++ sal-api/src/main/java/com/atlassian/sal/api/license/LicenseHandler.java
@@ -14,4 +14,18 @@ public interface LicenseHandler
      * @throws IllegalArgumentException if the license string is not a valid license
      */
     void setLicense(String license);
+
+    /**
+     * Gets the server ID of the currently running application.
+     *
+     * @return the server ID.
+     */
+    String getServerId();
+
+    /**
+     * Gets the Support Entitlement Number (SEN) for the currently running application.
+     *
+     * @return the Support Entitlement Number, or {@code null} if there is no current support entitlement.
+     */
+    String getSupportEntitlementNumber();
 }
